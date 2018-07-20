8747d2efcde62c56ce06c04d5f9b4cbcba043823
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-07-21 00:13:59 +0100

    Chore: computing the sound

--- audioSignalProcessing.py
+++ audioSignalProcessing.py
@@ -1,6 +1,7 @@
 import numpy as np  #import numpy
 import matplotlib.pyplot as plt #impport pyplot
 from scipy.io.wavfile import read #import package for reading sound files
+from scipy.io.wavfile import write #function for writing sound file
 
 a = np.array([0,1,2,3,4,5,6,7,8]) 
 
@@ -9,7 +10,7 @@ b = a[::-1] #reverse of array a
 print(b)
 
 plt.plot(b) #ploting the array a
-plt.show()
+#plt.show()
 
 (fs, x) = read('sound.wav') #read soud file called sound
 print(fs) #print the sample rate of file
@@ -20,3 +21,4 @@ print(size)
 y = x[44100:45100]
 plt.plot(y)
 plt.show()
+
