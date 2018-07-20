3c99cda7d224e3cf13f1cab33dccf452ec263069
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-07-20 23:34:20 +0100

    Feat: import package for reading sound files

--- audioSignalProcessing.py
+++ audioSignalProcessing.py
@@ -1,5 +1,6 @@
 import numpy as np  #import numpy
 import matplotlib.pyplot as plt #impport pyplot
+from scipy.io.wavfile import read #import package for reading sound files
 
 a = np.array([0,1,2,3,4,5,6,7,8]) 
 
