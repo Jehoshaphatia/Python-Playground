2062838522f0fff398defd8de2be76f53daf17cd
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-07-20 23:31:07 +0100

    Chore: Add comment

--- audioSignalProcessing.py
+++ audioSignalProcessing.py
@@ -1,7 +1,7 @@
 import numpy as np  #import numpy
 import matplotlib.pyplot as plt #impport pyplot
 
-a = np.array([0,1,2,3,4,5,6,7,8,9]) 
+a = np.array([0,1,2,3,4,5,6,7,8]) 
 
 b = a[::-1] #reverse of array a
 
