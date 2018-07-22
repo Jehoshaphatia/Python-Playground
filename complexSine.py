de08c35cc5d7eb72ca63a88018a3ff6e0c444980
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-07-22 09:22:37 +0100

    Feat: Add complex sine

--- complexSine.py
+++ complexSine.py
@@ -4,11 +4,11 @@ import numpy as np
 N = 500
 k = 3
 n = np.arange(-N/2, N/2)
-s = np.exp(ij * 2 * np.pi * k *n / N)
+s = np.exp(1j * 2 * np.pi * k *n / N)
 
 #complex sine wave
-plt.plot(n, np.real(s))
-plt.axis(-N/2, N/2-1, -1, 1)
+plt.plot(n, np.imag(s))
+plt.axis([-N/2, N/2-1, -1, 1])
 plt.xlabel('n')
 plt.ylabel('Amplitude')
 plt.show()
