cbf3319de8153146c789a712190d5db5f79f8a05
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-07-22 09:06:33 +0100

    Chore: fix error

--- sine.py
+++ sine.py
@@ -6,7 +6,7 @@ f0 = 1000
 phi = np.pi/2
 fs = 44100 
 t = np.arange (-0.002, .002, 1.0/fs)
-x = A * cos (2 * np.pi * f0 * t * phi)
+x = A * np.Cos (2 * np.pi * f0 * t * phi)
 plt.plot(t, x)
 plt.axis([-.002, .002, -.8, .8])
 plt.xlabel('Time')
