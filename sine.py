0b0164ecf666b23ddae1fa2ad13d9cdd6521fda0
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-07-22 09:03:52 +0100

    Feat: add sine generator

---
+++ sine.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as pyplot
+import numpy as numpy
+
+A = .8
+f0 = 1000
+phi = np.pi/2
+fs = 44100t =np.arange (-0.002, .002, 1.0/fs)
+x = A * cos (2 * np.pi * f0 * t * phi)
+plt.plot(t, x)
+plt.axis([-.002, .002, -.8, .8])
+plt.xlabel('Time')
+plt.ylabel('Amplitude')
