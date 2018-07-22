78f0e2aea2f1de57918daa8bbfd5a91a7615013d
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-07-22 10:38:37 +0100

    Feat: Add DFT

--- DFT.py
+++ DFT.py
@@ -5,13 +5,13 @@ N = 64
 k0 = 7
 x = np.exp(1j * 2 * np.pi * k0 / N * np.arange(N)) 
 
-x = np.array([])
+x = np.array([ ])
 
 for k in range(N):
      s = np.exp(1j * 2 * np.pi * k / N * np.arange(N))
      x = np.append(x, sum(x*np.conjugate(s)))
 
-plt.plot(np.arange(N), abs(x))
+plt.plot (np.arange(N), abs(x))
 plt.axis([0, N-1, 0, N])
 
 plt.show()
