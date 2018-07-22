e89af01c347fa1a535bbe716d4f22c9936647f66
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-07-22 09:05:23 +0100

    Chore: fix error

--- sine.py
+++ sine.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as pyplot
-import numpy as numpy
+import numpy as np
 
 A = .8
 f0 = 1000
 phi = np.pi/2
-fs = 44100t =np.arange (-0.002, .002, 1.0/fs)
+fs = 44100 
+t = np.arange (-0.002, .002, 1.0/fs)
 x = A * cos (2 * np.pi * f0 * t * phi)
 plt.plot(t, x)
 plt.axis([-.002, .002, -.8, .8])
