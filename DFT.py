868f69335b77ff0dfdea71e08abc3e716806321d
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-07-22 10:30:57 +0100

    Feat: Add DFT

--- DFT.py
+++ DFT.py
@@ -1,10 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+N = 64
+k0 = 7
+x = np.exp(1j * 2 * np.pi * k0 / N * np.arange(N)) 
+
 x = np.array([])
+
  for k in range(N):
-     s = np.exp(1j * 2 * pi * k / N * np.arange(N))
+     s = np.exp(1j * 2 * np.pi * k / N * np.arange(N))
      x = np.append(x, sum(x*np.conjugate(s)))
+
+plt.plot(np.arange(N), abs(x))
+plt.axis([0, N-1, 0, N])
+
+plt.show()
+
+
 
 
 
